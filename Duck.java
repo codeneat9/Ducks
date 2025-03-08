c3d81b186db811422cf402cde830c25a615c7df2
codeneat9 <yashwithbehara18@gmail.com>
2025-03-08 11:48:24 +0530

    calling fly for ducks that fly

--- Duck.java
+++ Duck.java
@@ -5,3 +5,7 @@ public abstract class Duck {
     FlyBehavior flyBehavior;
     SwimBehavior swimBehavior;
     QuackBehavior quackBehavior;
+
+    public void performFly() {//to fly
+        flyBehavior.fly();
+    }
