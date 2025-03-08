5c1a9bc564a218de81b151f6c7a1650e31e28f29
codeneat9 <yashwithbehara18@gmail.com>
2025-03-08 11:51:25 +0530

    Swim method

--- Swim.java
+++ Swim.java
@@ -2,3 +2,7 @@
 
 // This is the Swim class. It implements the SwimBehavior interface.
 public class Swim implements SwimBehavior {
+    public void swim() {
+        System.out.println("I can swim.");
+    }
+}
