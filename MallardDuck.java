f4148800549c914ea2d93476788be4de658fd4d2
codeneat9 <yashwithbehara18@gmail.com>
2025-03-08 12:03:17 +0530

    Mallard ducks behavior

--- MallardDuck.java
+++ MallardDuck.java
@@ -1,2 +1,7 @@
 // MallardDuck.java
 public class MallardDuck extends Duck {
+    public MallardDuck() {
+        flyBehavior = new FlyWithWings();
+        swimBehavior = new Swim();
+        quackBehavior = new Quack();
+    }
