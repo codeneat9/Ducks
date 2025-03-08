919aed5d03edfa0b5cda2e3272d990278ddaf2d2
codeneat9 <yashwithbehara18@gmail.com>
2025-03-08 11:54:37 +0530

    Quack method called for Squeaking ducks

--- Squeak.java
+++ Squeak.java
@@ -2,3 +2,7 @@
 
 // This is the Squeak class. It implements the QuackBehavior interface.
 public class Squeak implements QuackBehavior {
+    public void quack() {
+        System.out.println("Squeak! Squeak!");
+    }
+}
