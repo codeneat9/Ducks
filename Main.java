afe3600403cec88b46d14cd8f00c63f0a628cf22
codeneat9 <yashwithbehara18@gmail.com>
2025-03-08 11:40:43 +0530

    Main method

--- Main.java
+++ Main.java
@@ -5,3 +5,9 @@
 
 // Initializing the Main class
 public class Main {
+    // Main method
+    public static void main(String[] args) {
+        Duck mallard = new MallardDuck();
+        Duck redhead = new RedheadDuck();
+        Duck rubber = new RubberDuck();
+        Duck decoy = new DecoyDuck();
