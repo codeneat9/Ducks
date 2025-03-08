4f45fdd97695014ead318a99b9025957c80e6969
codeneat9 <yashwithbehara18@gmail.com>
2025-03-08 11:46:26 +0530

    Prints drowning for ducks that drown

--- Drown.java
+++ Drown.java
@@ -1,3 +1,8 @@
 // Drown.java
 // This class implements the SwimBehavior interface.
 public class Drown implements SwimBehavior {
+    // This method prints a message to the console.
+    public void swim() {
+        System.out.println("Oh! I'm drowning!");
+    }
+}
