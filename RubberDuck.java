19e7aa17705e8b867a7b34b71862875ea81f62fd
codeneat9 <yashwithbehara18@gmail.com>
2025-03-08 12:07:36 +0530

    Rubber duck's behavior

--- RubberDuck.java
+++ RubberDuck.java
@@ -2,3 +2,13 @@
 
 // This is the RubberDuck class. It extends the Duck class.
 public class RubberDuck extends Duck {
+    public RubberDuck() {
+        flyBehavior = new FlyNoWay();
+        swimBehavior = new FloatSwim();
+        quackBehavior = new Squeak();
+    }
+
+    public void display() {
+        System.out.println("I am a Rubber Duck.");
+    }
+}
