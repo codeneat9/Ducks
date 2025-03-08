0d098f1392804a005e57c3f9a80cc169d45bb475
codeneat9 <yashwithbehara18@gmail.com>
2025-03-08 12:06:38 +0530

    Displaying Red head duck's behavior

--- RedheadDuck.java
+++ RedheadDuck.java
@@ -2,3 +2,13 @@
 
 // This is the RedheadDuck class. It extends the duck class
 public class RedheadDuck extends Duck {
+    public RedheadDuck() {
+        flyBehavior = new FlyWithWings();
+        swimBehavior = new Swim();
+        quackBehavior = new Quack();
+    }
+
+    public void display() {
+        System.out.println("I am a Redhead Duck.");
+    }
+}
